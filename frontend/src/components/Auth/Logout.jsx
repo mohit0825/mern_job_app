951e6c4b7703f72281a7c8a87bb6216d8936d1d5
shubhs <shubhamiit9162@gmial.com>
2024-07-13 11:46:07 +0530

    first commit

--- frontend/src/components/Auth/Logout.jsx
+++ frontend/src/components/Auth/Logout.jsx
@@ -7,7 +7,7 @@ const Logout = () => {
 
   const handleLogout = async () => {
     try {
-      const response = await axios.get('/api/v1/user/logout'); // Ensure this matches the backend route
+      const response = await axios.get('http://127.0.0.1:4000/api/v1/user/logout'); // Ensure this matches the backend route
       if (response.status === 200) {
         localStorage.removeItem('authToken');
         history.push('/login');
